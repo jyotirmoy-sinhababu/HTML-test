bce376d7d250ca84a7c8f3993862cfb305ed8a01
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-11-17 17:24:27 +0530

    css added

--- src/pages/main/MainContainer.js
+++ src/pages/main/MainContainer.js
@@ -4,6 +4,9 @@ import imgOne from '../../images/imageone.png';
 import firstPer from '../../images/personOne.png';
 import secPer from '../../images/personTwo.png';
 import thirdPer from '../../images/personThird.png';
+import hand from '../../images/hand.png';
+import arrow from '../../images/arrow.png';
+import hiring from '../../images/hiring.png';
 
 import Logo from '../../components/logo/Logo';
 import NavBar from '../../components/navbar/NavBar';
@@ -131,6 +134,136 @@ const MainContainer = () => {
             }}
           />
         </div>
+      </div>
+      <div className='third-txt-cnt'>
+        <TextContainer
+          styleTwo={{
+            height: 'fit-content',
+            width: '820px',
+          }}
+          style={{ fontSize: '28px' }}
+          text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        />
+      </div>
+
+      <div className='main-small-card-cnt'>
+        <div className='small-card-cnt'>
+          {' '}
+          <div className='small-logo-txt-ctn'>
+            <ImgCnt img={hand} />
+            <div className='small-txt-ctn'>
+              {' '}
+              <TextContainer
+                styleTwo={{
+                  height: 'fit-content',
+                  width: '420px',
+                }}
+                style={{ fontSize: '28px' }}
+                text={'Lorem ipsum dolor sit.'}
+              />
+            </div>
+          </div>
+          <TextContainer
+            styleTwo={{
+              height: 'fit-content',
+              width: '420px',
+            }}
+            text={
+              ' adipisci veritatis enim odio fugiat nesciunt repellendus numquam'
+            }
+          />
+          <ButtonCnt
+            style={{
+              width: '167px',
+              height: '45px',
+              background: '#1492E6 0% 0% no-repeat padding-box',
+              boxShadow: '0px 16px 48px #00000029',
+              borderRadius: '33px',
+              border: 'none',
+            }}
+          />
+        </div>
+
+        <div className='small-card-cnt-two'>
+          {' '}
+          <div className='small-logo-txt-ctn'>
+            <ImgCnt img={arrow} />
+            <div className='small-txt-ctn'>
+              <TextContainer
+                styleTwo={{
+                  height: 'fit-content',
+                  width: '420px',
+                }}
+                style={{ fontSize: '28px' }}
+                text={'Lorem ipsum dolor sit.'}
+              />
+            </div>
+          </div>
+          <TextContainer
+            styleTwo={{
+              height: 'fit-content',
+              width: '420px',
+            }}
+            text={
+              ' adipisci veritatis enim odio fugiat nesciunt repellendus numquam'
+            }
+          />
+          <ButtonCnt
+            style={{
+              width: '167px',
+              height: '45px',
+              background: '#1492E6 0% 0% no-repeat padding-box',
+              boxShadow: '0px 16px 48px #00000029',
+              borderRadius: '33px',
+              border: 'none',
+            }}
+          />
+        </div>
+        <div className='small-card-cnt-three'>
+          {' '}
+          <div className='small-logo-txt-ctn'>
+            <ImgCnt img={hiring} />
+            <div className='small-txt-ctn'>
+              <TextContainer
+                styleTwo={{
+                  height: 'fit-content',
+                  width: '420px',
+                }}
+                style={{ fontSize: '28px' }}
+                text={'Lorem ipsum dolor sit.'}
+              />
+            </div>
+          </div>
+          <TextContainer
+            styleTwo={{
+              height: 'fit-content',
+              width: '420px',
+            }}
+            text={
+              ' adipisci veritatis enim odio fugiat nesciunt repellendus numquam'
+            }
+          />
+          <ButtonCnt
+            style={{
+              width: '167px',
+              height: '45px',
+              background: '#1492E6 0% 0% no-repeat padding-box',
+              boxShadow: '0px 16px 48px #00000029',
+              borderRadius: '33px',
+              border: 'none',
+            }}
+          />
+        </div>
+      </div>
+      <div className='fifth-txt-ctn'>
+        <TextContainer
+          styleTwo={{
+            height: 'fit-content',
+            width: '820px',
+          }}
+          style={{ fontSize: '28px' }}
+          text={'Lorem ipsum dolor sit.'}
+        />
       </div>
     </div>
   );
